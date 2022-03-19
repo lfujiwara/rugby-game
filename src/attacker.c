5e17e75417d0f2dfdae06c04247d4be4b01ae8c8
Lucas Toshio Löschner Fujiwara <lucastfujiwara@gmail.com>
2022-03-19 19:16:13 -0300

    feat: atk/def mvp

--- src/attacker.c
+++ src/attacker.c
@@ -1,7 +1,10 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
+#include "dimension.h"
 #include "direction.h"
 #include "position.h"
 #include "spy.h"
@@ -11,19 +14,23 @@
 
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
+const direction_t DIRS_ATK[] = {DIR_UP,         DIR_UP_RIGHT, DIR_RIGHT,
+                                DIR_DOWN_RIGHT, DIR_DOWN,     DIR_DOWN_LEFT,
+                                DIR_LEFT,       DIR_UP_LEFT};
+
+#define DIRS_ATK_LEN (sizeof(DIRS_ATK) / sizeof(direction_t))
 
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
-direction_t execute_attacker_strategy(
-    position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
+direction_t execute_attacker_strategy(position_t attacker_position,
+                                      Spy defender_spy) {
   UNUSED(attacker_position);
   UNUSED(defender_spy);
 
   // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  return (direction_t)DIR_RIGHT;
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
@@ -11,19 +13,24 @@
 
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
+const direction_t DIRS_DEF[] = {DIR_UP,         DIR_UP_RIGHT, DIR_RIGHT,
+                                DIR_DOWN_RIGHT, DIR_DOWN,     DIR_DOWN_LEFT,
+                                DIR_LEFT,       DIR_UP_LEFT};
+
+#define DIRS_DEF_LEN (sizeof(DIRS_DEF) / sizeof(direction_t))
 
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
-direction_t execute_defender_strategy(
-    position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
+direction_t execute_defender_strategy(position_t defender_position,
+                                      Spy attacker_spy) {
+
   UNUSED(defender_position);
   UNUSED(attacker_spy);
 
   // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  return (direction_t)DIR_LEFT;
 }
 
 /*----------------------------------------------------------------------------*/
